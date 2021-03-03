447b5650339026cf91f92208d448261dd188bb79
Muhammadsiddiq <muhammadsiddiqbe@gmail.com>
2021-03-03 15:32:04 +0500

    initial commit

---
+++ psql.js
@@ -0,0 +1,41 @@
+const { Pool } = require("pg")
+
+const pool = new Pool({
+  host: "localhost",
+  user: "user",
+  password: "pass",
+  database: "mydb",
+})
+
+const fetch = async (SQL, ...params) => {
+
+  const client = await pool.connect()
+
+  try {
+
+    const { rows } = await client.query(SQL, params)
+
+    return rows
+  }
+  finally {
+    client.release()
+  }
+}
+
+const fetchOne = async (SQL, ...params) => {
+
+  const client = await pool.connect()
+
+  try {
+
+    const { rows: [row] } = await client.query(SQL, params)
+
+    return row
+  }
+  finally {
+    client.release()
+  }
+}
+
+module.exports.fetch = fetch
+module.exports.fetchOne = fetchOne
